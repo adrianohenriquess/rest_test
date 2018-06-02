edb4f21755b17630a252f16afc3e54307f21a7d8
adriano_henrique <adriano.henriquesds@gmail.com>
2018-06-01 21:12:15 -0300

    cliente java comum

---
+++ src/main/java/com/example/rest_test/client/HelloClient.java
@@ -0,0 +1,26 @@
+package com.example.rest_test.client;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+
+public class HelloClient {
+
+	WebTarget webTarget;
+
+	public HelloClient() {
+		Client client = ClientBuilder.newClient();
+		webTarget = client.target("http://localhost:8080/rest_test/rest/hello");
+	}
+
+	public String getUriAsString() {
+		return webTarget.getUri().toString();
+	}
+
+	public String sayHello(String message) {
+		Invocation webServiceCall = webTarget.request().accept("text/plain").build("PUT", Entity.text(message));
+		return webServiceCall.invoke(String.class);
+	}
+}
